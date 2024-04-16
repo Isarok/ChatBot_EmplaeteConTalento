367aaad48f44cc495580bd695a2efe1492c73ae6
Hack3r-han <hannahphil27@gmail.com>
2024-04-16 11:39:02 +0200

    Upload Landing Page

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,9 +1,24 @@
+import Nav from "../../components/Nav";
+import Banner from "../../components/Banner";
+import backgroundImage from "../../assets/BackgroundImg.jpeg"; 
+// import Games from "../../components/GameCategories"
+
 const Home = () => {
-    return (
-      <div>
-        Hola soy la Homi 🤖
+  return (
+    <main
+      className="w-full h-auto"
+      style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover' }}
+    >
+      <Nav />
+      <div className="p-20 w-full h-auto"
+      style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover' }}>
+        <div className="max-w-screen-xl mx-auto">
+          <Banner />
+          {/* <Games /> */}
+        </div>
       </div>
-    )
-  }
-  
-  export default Home
+    </main>
+  );
+};
+
+export default Home;
