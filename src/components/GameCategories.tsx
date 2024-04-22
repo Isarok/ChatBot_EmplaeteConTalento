c8cab96f9d5b8961b86869623fac7ae1ff15a5c2
Isarok <isaac.lucerito@gmail.com>
2024-04-22 22:04:45 +0200

    add styles on LandingPage

--- src/components/GameCategories.tsx
+++ src/components/GameCategories.tsx
@@ -7,7 +7,7 @@ const Games = () => {
   return (
     <section className="font-sans lgl:w-1/3 lg:w-1/2 backdrop-blur-2xl pt-5 flex flex-col rounded-lg border border-gray-300">
       <div className="flex flex-col gap-3">
-        <h1 className="text-center">¿PREPARAD@?</h1>
+        <h1 className=" text-xl text-center text-slate-300 font-bold ">¿PREPARAD@?</h1>
         <div className="flex lgl:flex-row lg:flex-row md:flex-row flex-col items-center sm:items-center sm:justify-start p-5">
           <Link to="/orange-page" className="link-wrapper">
             <div className="image-link bg-red-700 hover:bg-red-600 flex items-center justify-center w-28 h-28 text-white font-bold rounded-full border-2 border-red-800">
@@ -15,8 +15,8 @@ const Games = () => {
             </div>
           </Link>
           <div className="ml-4">
-            <h1 className="text-left max-w-xs">JUEGO DE CV</h1>
-            <p className="text-sm max-w-xs">Descubre si tu currículum está bien elaborado</p>
+            <h1 className="text-left text-xl text-slate-300 font-semibold max-w-xs">JUEGO DE CV</h1>
+            <p className="text-sm text-slate-300 font-semibold max-w-xs">Descubre si tu currículum está bien elaborado</p>
           </div>
         </div>
         <div className="flex lgl:flex-row lg:flex-row md:flex-row flex-col items-center sm:items-center sm:justify-start p-5">
@@ -26,8 +26,8 @@ const Games = () => {
             </div>
           </Link>
           <div className="ml-4">
-            <h1 className="text-left max-w-xs">JUEGO DE COMPETENCIAS</h1>
-            <p className="text-sm max-w-xs">Descubre los talentos que desarrollas con tus hobbies</p>
+            <h1 className="text-left text-xl text-slate-300 font-semibold max-w-xs">JUEGO DE COMPETENCIAS</h1>
+            <p className="text-sm text-slate-300 font-semibold  max-w-xs">Descubre los talentos que desarrollas con tus hobbies</p>
           </div>
         </div>
         <div className="flex lgl:flex-row lg:flex-row md:flex-row flex-col items-center sm:items-center sm:justify-start p-5">
@@ -37,8 +37,8 @@ const Games = () => {
             </div>
           </Link>
           <div className="ml-4">
-            <h1 className="text-left max-w-xs">JUEGO DE EMPRENDIMIENTO</h1>
-            <p className="text-sm max-w-xs">Descubre si tienes cualidades para emprender</p>
+            <h1 className="text-left text-xl text-slate-300 font-semibold max-w-xs">JUEGO DE EMPRENDIMIENTO</h1>
+            <p className="text-sm text-slate-300 font-semibold max-w-xs">Descubre si tienes cualidades para emprender</p>
           </div>
         </div>
       </div>
